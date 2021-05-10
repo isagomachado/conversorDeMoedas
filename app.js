2001646dc1b31b55ddd8025737605ea4beaedfa0
isagomachado <82418322+isagomachado@users.noreply.github.com>
2021-05-10 19:49:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var  valorEmDolarTexto = prompt("Qual o valor em dolar você quer converter?")
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+var valorEmReal = valorEmDolarNumero * 5.50
+var valorEmRealDecimal = valorEmReal.toFixed(2)
+alert(valorEmRealDecimal)
+
+//Revisão
+//variáveis var int - float - string
+//alert - parseint - parsefloat - prompt
+//operação + somar * multiplicar
+//comentario final
